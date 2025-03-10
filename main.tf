5de09e4f448cb6cf326a47468b52228f0cd45842
Kowsalya-k13 <kowsalya.kumar@batonsystems.com>
2025-03-10 16:04:25 +0530

    added outputs from vpc module

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "inter_vpc" {
     source     = "./modules/vpc"
 }
 
-module "iam_role" {
-    source     = "./modules/iam_role"
-}
+# module "iam_role" {
+#     source     = "./modules/iam_role"
+# }
 
 module "aws_ec2" {
     source     = "./modules/aws_ec2"
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,13 @@
 output "vpc_id" {
   value = aws_vpc.intern_vpc.id
 }
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+output "igw_id" {
+  value = aws_internet_gateway.intern-igw.id
+}
