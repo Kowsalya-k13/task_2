2057bc9c418e80cb4775578f88cfe3f50d0b8d4d
Kowsalya-k13 <kowsalya.kumar@batonsystems.com>
2025-03-10 16:30:09 +0530

    added NAT

--- modules/route_tables/main.tf
+++ modules/route_tables/main.tf
@@ -18,3 +18,29 @@ resource "aws_route_table_association" "public_association" {
     subnet_id = var.intern_public_subnet
     route_table_id = aws_route_table.pub-RT.id
 }
+
+#NAT 
+resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+    tags = {
+        Name        = "kowsalya-EIP"
+        Project     = "Terraform-Task"
+        Owner       = "Kowsalya"
+        Purpose     = "Creating EIP for NAT"
+        Created_On  = "March-10"
+    }
+    
+}
+
+resource "aws_nat_gateway" "internNat" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = var.intern_private_subnet
+
+    tags = {
+        Name        = "InternNAT"
+        Project     = "Terraform-Task"
+        Owner       = "Kowsalya"
+        Purpose     = "Creating NAT inside Private Subnet"
+        Created_On  = "March-10"
+    }
+}
