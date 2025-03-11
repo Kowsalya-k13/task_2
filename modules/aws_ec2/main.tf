9dca38e879a057da6a770afcdec8171c16a21b38
Kowsalya-k13 <kowsalya.kumar@batonsystems.com>
2025-03-11 11:50:19 +0530

    added ec2

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -27,3 +27,26 @@ resource "aws_security_group" "intern_sg" {
 
   }
 }
+
+resource "aws_instance" "ec2inPublic" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_pair
+    subnet_id = var.subnet_id
+    vpc_security_group_ids  = [aws_security_group.intern_sg.id]
+
+    root_block_device {
+        volume_size           = 8
+        delete_on_termination = true
+        encrypted             = true
+        kms_key_id            = var.kms_key_id
+    }
+
+    tags = {
+        Name        = "Instance-in-public"
+        Project     = "Terraform-Task"
+        Owner       = "Kowsalya"
+        Purpose     = "Creating instance inside Public Subnet as instructed"
+        Created_On  = "March-11"
+    }
+}
