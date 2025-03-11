0d21626eb30e0b551cbeca07d97e1465c147164c
Kowsalya-k13 <kowsalya.kumar@batonsystems.com>
2025-03-11 12:41:24 +0530

    added rds

---
+++ modules/rds/main.tf
@@ -0,0 +1,38 @@
+#We need subnet group for RDS 
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "intern-rds-subnet-group"
+  subnet_ids = [var.subnet_id]
+
+  tags = {
+    Name = "Intern RDS Subnet Group"
+  }
+}
+#RDS instance
+resource "aws_db_instance" "low_cost_rds" {
+  identifier           = "intern-rds"
+  engine              = "mysql"  
+  engine_version      = "8.0"    
+  instance_class      = "db.t3.micro"  
+  allocated_storage   = 20  
+  storage_type        = "gp2"
+  storage_encrypted   = true
+  kms_key_id          = var.rds_kms_key_id
+
+  username            = var.db_username
+  password            = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  multi_az           = false 
+  publicly_accessible = false
+  backup_retention_period = 7
+  skip_final_snapshot = true
+
+  tags = {
+    Name        = "Private RDS Instance"
+    Project     = "Terraform-RDS"
+    Owner       = "Kowsalya"
+    Purpose     = "RDS in Private Subnet"
+    Created_On  = "March-11"
+  }
+}
---
+++ modules/rds/variables.tf
@@ -0,0 +1,22 @@
+variable "subnet_id" {
+  type = string
+  default = "subnet-08cb7ba68adfcb996"  
+  description = "Subnet id of private subnet"   
+}
+
+variable "rds_kms_key_id" {
+  type = string
+  default = "38861bc3-f045-4fc6-9253-aa67ad4637b4"     
+  description = "KMS key id for RDS"
+}
+
+variable "db_username" {
+  type = string
+  default = "kowsalya"     
+  description = "Username for DB"
+}
+variable "db_password" {
+  type = string
+  default = "kowsi@rds"     
+  description = "Password for DB"
+}
